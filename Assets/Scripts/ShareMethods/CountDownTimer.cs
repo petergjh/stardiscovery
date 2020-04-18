8040f8fcb0265b04a75e0a98d79c5cfee6e9a573
Peter <306739889@qq.com>
2020-04-18 15:37:25 +0800

    count down timer

---
+++ Assets/Scripts/ShareMethods/CountDownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UIFrame;
+using DemoProject;
+using UnityEngine.UI;
+
+
+    public class CountDownTimer : MonoBehaviour
+    {
+        public GameObject text;
+        public int TotalTime = 60;
+
+        void Start()
+        {
+
+            StartCoroutine(CountDown());
+
+        }
+
+        IEnumerator CountDown()
+        {
+            while (TotalTime >= 0)
+            {
+                text.GetComponent<Text>().text = TotalTime.ToString();
+                yield return new WaitForSeconds(1);
+                TotalTime--;
+
+            }
+        }
+
+    }
+
