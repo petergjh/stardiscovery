9a4bab0c31dc1a2723fa05f142f431aeb05f8666
Peter <306739889@qq.com>
2020-04-18 19:43:45 +0800

    count down timer两种方法：协程方法，Update方法

--- Assets/Scripts/ShareMethods/CountDownTimer.cs
+++ Assets/Scripts/ShareMethods/CountDownTimer.cs
@@ -8,35 +8,69 @@ using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
 {
-    public GameObject text;
-    public int TotalTime = 5;
+    private int totaltime1 = 5;
+    private int totaltime2 = 6;
+    private float intervaletime=1;
+    public Text countdown1text;
+    public Text countdown2text;
 
     void Start()
     {
-
+        countdown1text.text = string.Format("{0:00}:{1:00}", (int)totaltime1 / 60, (float)totaltime1 % 60);
+        countdown2text.text = string.Format("{0:00}:{1:00}", (int)totaltime2 / 60, (float)totaltime2 % 60);
         StartCoroutine(CountDown());
-
     }
 
+    // 方法一：用IEnumerator协程迭代器实现倒计时器
     IEnumerator CountDown()
     {
 
 
-        while (TotalTime >=0)
+        while (totaltime1 >=0)
         {
-            int M = (int)(TotalTime / 60);
-            float S = TotalTime % 60;
+            int M = (int)(totaltime1 / 60);
+            float S = totaltime1 % 60;
 
-            //text.GetComponent<Text>().text = TotalTime.ToString();
-            text.GetComponent<Text>().text = M + ":" + string.Format("{0:00}", S);
+            // 显示格式为 M分：S秒
+            countdown1text.GetComponent<Text>().text = string.Format("{0:00}:{1:00}", M,S);
+            // 每一帧update后等待1秒延迟再继续下一帧
             yield return new WaitForSeconds(1);
-            TotalTime--;
+            // 时间减去一秒
+            totaltime1--;
 
-            if (TotalTime < 0)
+            // 计时器复位重新开始计时
+
+            if (totaltime1 < 0)
             {
-                TotalTime = 6;
+                totaltime1 = 5;
             }
         }
+    }
+
+
+    // 方法二：采用Update方法实现倒计时
+    // Update is called once per frame
+    void Update()
+    {
+        
+            int M = (int)(totaltime2 / 60);
+            float S = totaltime2 % 60;
+            if (totaltime2 > 0)
+            {
+                intervaletime -= Time.deltaTime;
+                if (intervaletime <= 0)
+                {
+                    intervaletime += 1;
+                    totaltime2--;
+                    countdown2text.text = string.Format("{0:00}:{1:00}", M, S);
+
+                }
+            }
+            if (totaltime2 <= 0)
+            {
+                totaltime2 = 6;
+            }
+        
     }
 
 }
