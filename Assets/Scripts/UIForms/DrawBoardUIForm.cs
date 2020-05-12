af93a93de0490b2c12b77967a826b72f53de39d8
Peter <306739889@qq.com>
2020-05-12 17:31:20 +0800

    公告栏

--- Assets/Scripts/UIForms/DrawBoardUIForm.cs
+++ Assets/Scripts/UIForms/DrawBoardUIForm.cs
@@ -7,9 +7,23 @@ using DG.Tweening;
 
 namespace DemoProject
 {
+    //纸张正反面
+    public enum PageState
+    {
+        Front,
+        Back
+    }
 
     public class DrawBoardUIForm : BaseUIForm
     {
+
+        public GameObject mFront ;//纸张正面
+        public GameObject mBack;//纸张背面
+        public PageState mPageState = PageState.Front;//纸张当前的状态，是正面还是背面？
+        private float mTime =0.5f;
+        private bool isActive = false;//true代表正在执行翻转，不许被打断
+
+        #region 事件注册
         private void Awake()
         {
             Debug.Log("公告栏窗体DrawBoardUIForm开始初始化");
@@ -23,6 +37,17 @@ namespace DemoProject
             RigisterButtonObjectEvent("Btn_Close",
                 p=> CloseUIForm()
                 );
+
+            // 注册按钮事件
+            RigisterButtonObjectEvent("Btn_zhi1",
+                p =>StartBack()
+                );
+
+            // 注册按钮事件
+            RigisterButtonObjectEvent("Btn_zhi2",
+                p =>StartFront()
+                );
+
 
             // 注册道具事件：神杖
             RigisterButtonObjectEvent("BtnTicket",
@@ -74,6 +99,109 @@ namespace DemoProject
 
 
         }
+        #endregion 事件注册结束
+
+        /// <summary>
+        /// 初始化纸张角度，根据mPageState
+        /// </summary>
+        public void Init()
+        {
+            mFront = UnityHelper.FindTheChildNode(this.gameObject, "Btn_zhi1").gameObject;
+            mBack = UnityHelper.FindTheChildNode(this.gameObject, "Btn_zhi2").gameObject;
+            if (mPageState == PageState.Front)
+            {
+                //如果是从正面开始，则将背面旋转90度，这样就看不见背面了
+                mBack.transform.eulerAngles = new Vector3(0, 90, 0);
+                mFront.transform.eulerAngles = Vector3.zero;
+            }
+            else
+            {
+                //从背面开始，同理
+                mFront.transform.eulerAngles = new Vector3(0, 90, 0);
+                mBack.transform.eulerAngles = Vector3.zero;
+            }
+        }
+        private void Start()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// 留给外界调用的接口
+        /// </summary>
+        public void StartBack()
+        {
+            if (isActive)
+                return;
+            StartCoroutine(ToBack());
+        }
+        /// <summary>
+        /// 留给外界调用的接口
+        /// </summary>
+        public void StartFront()
+        {
+            if (isActive)
+                return;
+            StartCoroutine(ToFront());
+        }
+
+        /// <summary>
+        /// 切换到后一页
+        /// </summary>
+        IEnumerator ToBack()
+        {
+            Debug.Log("翻到后页协程");
+            isActive = true;
+            mFront.transform.DOBlendableLocalMoveBy(new Vector3(400, -20, 0), mTime);
+            mBack.transform.DOPunchPosition(new Vector3(-80, 10, 0), mTime * 2, 0, 0);
+            yield return new WaitForSeconds(0.3f);
+            mBack.transform.SetAsLastSibling();
+            //mBack.transform.DOBlendablePunchRotation(new Vector3(10, 10, 0),1f, 1, 0);
+            mFront.transform.DOLocalMove(new Vector3(0, 0, 0), mTime);
+            isActive = false;
+
+        }
+        /// <summary>
+        /// 切换到首页
+        /// </summary>
+        IEnumerator ToFront()
+        {
+            isActive = true;
+            mBack.transform.DOMove(new Vector3(30, -8, 0), mTime);
+            yield return new WaitForSeconds(0.5f);
+            mFront.transform.DOPunchPosition(new Vector3(-20, 10, 0), mTime, 1,0);
+            mFront.transform.SetAsLastSibling();
+            mBack.transform.DOMove(new Vector3(0, 0, 0), mTime);
+            isActive = false;
+
+        }
+
+        ///// <summary>
+        ///// 翻转到背面
+        ///// </summary>
+        //IEnumerator ToBack()
+        //{
+        //    Debug.Log("翻到后页协程");
+        //    isActive = true;
+        //    mFront.transform.DORotate(new Vector3(0, 90, 0), mTime);
+        //    for (float i = mTime; i >= 0; i -= Time.deltaTime)
+        //        yield return 0;
+        //    mBack.transform.DORotate(new Vector3(0, 0, 0), mTime);
+        //    isActive = false;
+
+        //}
+        /// <summary>
+        /// 翻转到正面
+        /// </summary>
+        //IEnumerator ToFront()
+        //{
+        //    isActive = true;
+        //    mBack.transform.DORotate(new Vector3(0, 90, 0), mTime);
+        //    for (float i = mTime; i >= 0; i -= Time.deltaTime)
+        //        yield return 0;
+        //    mFront.transform.DORotate(new Vector3(0, 0, 0), mTime);
+        //    isActive = false;
+        //}
 
     }
 
